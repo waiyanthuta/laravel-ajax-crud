9b199aa87a598f09a8a69e30b7d9df24bde72c02
waiyan <khantkhantko899@gmail.com>
2022-12-05 03:55:15 +0630

    [feat]Add and Show feature

---
+++ app/Http/Controllers/StudentController.php
@@ -0,0 +1,107 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Student;
+use Illuminate\Http\Request;
+use DataTables;
+
+class StudentController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index(Request $request)
+    {
+        $students = Student::get();
+        if($request->ajax())
+        {
+            $allData = DataTables::of($students)
+            ->addIndexColumn()
+            ->addColumn('action',function($row){
+                $btn = '<a href="javascript:void(0)" data-toggle="tooltip" data-id="'.$row->id.'"
+                        data-original-title="Edit" class="btn btn-primary btn-sm edit editStudent">Edit</a>';
+                $btn.= '<a href="javascript:void(0)" data-toggle="tooltip" data-id="'.$row->id.'"
+                        data-original-title="Delete" class="btn btn-danger btn-sm delete deleteStudent">Delete</a>';
+                        return $btn;
+            })
+            ->rawColumns(['action'])
+            ->make(true);
+            return $allData;
+        }
+        return view('students',compact('students'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        Student::updateOrCreate(['id'=>$request->student_id],
+        [   
+            'name'=>$request->name,
+            'email'=>$request->email,
+        ]);
+        return response()->json(['success'=>'Student added successfully']);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
---
+++ resources/views/students.blade.php
@@ -0,0 +1,117 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Ajex Crud</title>
+    <!-- CSS only -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.datatables.net/1.13.1/css/jquery.dataTables.min.css" rel="stylesheet">
+</head>
+<body>
+    <div class="container">
+        <h1 class="text-center mt-3">Laravel Ajex Crud</h1>
+        <div class="card shadow mb-4">
+            <div class="card-header py-3">
+                <h6 class="m-0 font-weight-bold h3">Student List</h6>
+            </div>
+            <div class="card-body">
+                <div class="table-responsive">
+                    <table class="table table-bordered data-table" id="dataTable" width="100%" cellspacing="0">
+                        <a class="btn btn-success" id="createNewStudent" style="float:right" href="javascript:void(0)" >Add</a>
+                        <thead>
+                            <tr>
+                                <th>No.</th>
+                                <th>Name</th>
+                                <th>Email</th>
+                                <th>Action</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                              
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="modal fade" id="ajaxModal" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h4 class="modal-title" id="modalHeading"></h4>
+                </div>
+                <div class="modal-body">
+                    <form id="studentForm" name="studentForm" class="form-horizontal">@csrf
+                        <input type="hidden" name="student_id" id="student_id">
+                        <div class="form-group">
+                            Name: <br>
+                            <input type="text" class="form-control" id="name" name="name" placeholder="Enter Name" value="" required>
+                        </div>
+                        <div class="form-group">
+                            Email: <br>
+                            <input type="text" class="form-control" id="email" name="email" placeholder="Enter Email" value="" required>
+                        </div>
+                        <button type="submit" class="btn btn-success" id="saveBtn" value="Create">Save</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+ <!-- JavaScript Bundle with Popper -->
+ <script
+ src="https://code.jquery.com/jquery-3.6.1.min.js"
+ integrity="sha256-o88AwQnZB+VDvE9tvIXrMQaPlFFSUTR+nldQm1LuPXQ="
+ crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+    <script src="https://cdn.datatables.net/1.13.1/js/jquery.dataTables.min.js"></script>
+</body>
+<script type="text/javascript">
+    $(function(){
+        $.ajaxSetup({
+            headers:{
+                'X-CSRF-TOKEN' : $('meta[name="csrf-token"]').attr('content')
+            }
+        })
+        var table = $(".data-table").DataTable({
+            serverSide : true,
+            processing : true,
+            ajax : "{{route('students.index')}}",
+                columns : [
+                    {data : 'DT_RowIndex' , name : 'DT_RowIndex'},
+                    {data : 'name' , name : 'name'},
+                    {data : 'email' , name : 'email'},
+                    {data : 'action' , name : 'action'},
+                ]
+            });
+            $("#createNewStudent").click(function(){
+                $("#student_id").val();
+                $("#studentForm").trigger("reset");
+                $("#modalHeading").html('Add Student');
+                $("#ajaxModal").modal('show');
+            });
+            $("#saveBtn").click(function(e){
+                e.preventDefault()
+                $(this).html('Save');
+
+                $.ajax({
+                    data:$("#studentForm").serialize(),
+                    url:"{{route('students.store')}}",
+                    type:"POST",
+                    dataType:'json',
+                    success:function(data){
+                        $("#studentForm").trigger("reset");
+                        $('#ajaxModal').modal('hide');
+                        table.draw();
+                    },
+                    error:function(data){
+                        console.log('Error:',data);
+                        $("#saveBtn").html('Save')
+                    }
+                })
+            });
+            // $('body').on('click','.deleteStudent'.function(){})
+    })
+</script>
+</html>
